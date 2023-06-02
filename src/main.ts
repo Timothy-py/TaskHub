f09549cabb9c485ec778dec13303c5af13159f7d
Timothy-py <adeyeyetimothy33@gmail.com>
2023-06-02 23:52:55 +0100

    Doc<OpenAPI>:setup OpenAPI swagger documentation :memo:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // setup swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('TaskHub API')
+    .setDescription('Swagger OpenAPI documentation for TaskHub API')
+    .setVersion('1.0')
+    .setContact(
+      'Timothy',
+      'https://github.com/Timothy-py',
+      'adeyeyetimothy33@gmail.com',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
