257c7352093a6e477a56952949ab975cfc3ae3db
Timothy-py <adeyeyetimothy33@gmail.com>
2023-06-03 14:18:22 +0100

    feat<strategy>: implemented refresh token strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from 'src/user/user.model';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [SequelizeModule.forFeature([User]), JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService, Logger],
 })
